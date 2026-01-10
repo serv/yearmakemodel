60b883c252ab55df0c8bb813b36f922949402eea
Jason Kim <iamjsonkim@gmail.com>
2026-01-09 23:44:49 -0800

    use postgres prod env differently

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,5 +2,8 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL!, {
+  ssl: process.env.NODE_ENV === "production" ? "require" : false,
+  prepare: process.env.NODE_ENV === "production" ? false : undefined,
+});
 export const db = drizzle(client, { schema });
